b126da6c9d4acdea9a5be86fc82e2695ce0c7a59
allportfoliowebsite-design <allportfoliowebsite@gmail.com>
2026-09-13 21:40:48 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer: React.FC = () => {
   // Social link gulo ekhane update korte parbe
   const socials = [
-    { name: 'Facebook', icon: 'fab fa-facebook-f', href: 'https://www.facebook.com/pfazizulhakim' },
-    { name: 'Instagram', icon: 'fab fa-instagram', href: 'https://www.instagram.com/pfazizulhakim/?__pwa=1' },
+    { name: 'Facebook', icon: 'fab fa-facebook-f', href: 'https://www.facebook.com/profile.php?id=61591557341630' },
+    { name: 'Instagram', icon: 'fab fa-instagram', href: 'https://www.instagram.com/dmazizulhakim9' },
     { name: 'Twitter', icon: 'fab fa-x-twitter', href: 'https://x.com/AhmedRony586967' },
     { name: 'linkedin', icon: 'fab fa-linkedin', href: 'https://www.linkedin.com/in/azizulhakimrony' },
     { name: 'YouTube', icon: 'fab fa-youtube', href: 'https://youtube.com' },
